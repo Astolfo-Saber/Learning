8ce72eb9a7f096cefb58b418cfb75a1e0d5f104e
Astolfo-Saber <hungtd2180@gmail.com>
2023-12-27 16:24:28 +0700

    done

---
+++ codeSignal/src/exploringthewater/AreSimilar.java
@@ -0,0 +1,19 @@
+package exploringthewater;
+
+import java.util.Arrays;
+
+public class AreSimilar {
+    //Mô tả: vì chỉ được hoán vị 1 lần, thế nên số phần tử khác nhau ở 2 dãy tối đa là 2 ( 1 cặp hoán vị)
+    //Sau đó ta kiểm tra xem 2 dãy có phần tử gống nhau không phòng trường hợp 2 phần tử khác nhau ở 2 dãy là 4 số 3 4 số khác nhau
+    public boolean solution(int[] a, int[] b) {
+        int result = 0;
+        for (int i = 0; i < a.length; i++){
+            if (a[i] != b[i])
+                result++;
+        }
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return result <= 2 && Arrays.equals(a, b);
+    }
+
+}
