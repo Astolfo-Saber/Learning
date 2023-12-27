fc710632dcbd965b199523caf1fc326708f910aa
Astolfo-Saber <hungtd2180@gmail.com>
2023-12-27 15:57:55 +0700

    done

--- codeSignal/src/exploringthewater/AlternatingSums.java
+++ codeSignal/src/exploringthewater/AlternatingSums.java
@@ -1,4 +1,18 @@
 package exploringthewater;
 
 public class AlternatingSums {
+    //Mô tả: Duyệt mảng phần tử chẵn vào team 1, lẻ vào team 2
+    int[] solution(int[] a){
+        int teamOne = 0;
+        int teamTwo = 0;
+        for (int i = 0; i < a.length; i++){
+            if (i % 2 == 0){
+                teamOne+= a[i];
+            }else{
+                teamTwo+= a[i];
+            }
+        }
+        int [] result = {teamOne, teamTwo};
+    return result;
+    }
 }
